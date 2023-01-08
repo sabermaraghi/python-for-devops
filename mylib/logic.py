892258228e7f0c8330f139c70bde209a286fefd2
sabermaraghi <120035678+sabermaraghi@users.noreply.github.com>
2023-01-08 11:48:51 +0000

    adding a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -6,3 +6,11 @@ def wiki(name="War Goddes", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    """Search Wikipedia for names."""
+
+    results = wikipedia.search(name)
+    return results
+    
